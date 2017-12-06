a0e6a550bdda98d3823697fefb7445923929dc74
huangjunpeng@outlook.com <huangjunpeng@outlook.com>
2017-12-06 23:31:34 +0800

    1

---
+++ application/modules/admin/controllers/RoleController.php
@@ -0,0 +1,18 @@
+<?php
+class Admin_RoleController extends XlbController
+{
+    /**
+     * 柜子首页
+     */
+    public function indexAction()
+    {
+    }
+
+    public function roleManageAction()
+    {
+    }
+
+    public function cabiUserAction()
+    {
+    }
+}
