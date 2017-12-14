c4fde3ce8a1b595d289e13f991a96c5feb481213
junpeng.huang <junpeng.huang@mxsoft.com>
2017-12-14 18:12:46 +0800

    修改获取分类

--- application/models/XlbBookCategoryModel.php
+++ application/models/XlbBookCategoryModel.php
@@ -35,4 +35,19 @@ class XlbBookCategoryModel extends Xlb
     public function getAllCategorys() {
         return $this->fetchAll()->toArray();
     }
+
+    /**
+     * @return array
+     */
+    public function getCategorys() {
+        $table_1 = XlbBookCategoryMapModel::getInstance()->getDbName();
+        $table_2 = XlbBookModel::getInstance()->getDbName();
+        $select = $this->getAdapter()->select();
+        $select->from(array('t'=>$this->_name),array('bc_id','bc_name'))
+            ->join(array('t1'=>$table_1),'t.bc_id=t1.bc_id',array())
+            ->join(array('t2'=>$table_2),'t1.b_id=t2.b_id',
+                array('id'=>'b_id','name'=>'b_name','picture'=>'b_picture','b_score'))
+            ->order('b_score DESC');
+        return $this->getAdapter()->fetchAll($select);
+    }
 }
--- application/modules/app/controllers/BookController.php
+++ application/modules/app/controllers/BookController.php
@@ -19,4 +19,29 @@ class BookController extends PublicController
         );
         $this->xlb_ret(1, '', $allBook);
     }
+
+    /**
+     * 获取分类列表
+     */
+    public function getAction(){
+        $rows = XlbBookCategoryModel::getInstance()
+            ->getCategorys();
+        $types = array();
+        foreach ($rows as &$row){
+            if (!isset($types[$row['bc_id']])){
+                $types[$row['bc_id']]['bc_id'] = $row['bc_id'];
+                $types[$row['bc_id']]['bc_name'] = $row['bc_name'];
+                $types[$row['bc_id']]['books'][] = &$row;
+            } else {
+                $types[$row['bc_id']]['books'][] = &$row;
+            }
+            unset($row['bc_id']);
+            unset($row['bc_name']);
+        }
+        $rets = array();
+        foreach ($types as $type) {
+            $rets[] = $type;
+        }
+        $this->xlb_ret(1, '', $rets);
+    }
 }
